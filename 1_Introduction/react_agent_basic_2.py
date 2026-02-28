175c9b5e1f077a0f6e9c35bd0eae11827b1fec2a
Kevin Lee <kevint.lee0@gmail.com>
2026-02-28 12:28:54 -0800

    Add datetime tool

--- 1_Introduction/react_agent_basic_2.py
+++ 1_Introduction/react_agent_basic_2.py
@@ -1,19 +1,32 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from langchain.agents import create_agent 
+from langchain.agents import create_agent
+from langchain_core.tools import tool
 from langchain_tavily import TavilySearch
+import datetime
+
+@tool
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S") -> str:
+    """
+    Returns the current system date and time as a string. 
+    Use this when the user asks for 'today', 'now', or current time/date.
+    """
+    
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime(format)
+    return formatted_time
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
 
 search_tool = TavilySearch(max_results=3, search_depth="basic")
 
-tools = [search_tool]
+tools = [search_tool, get_system_time]
 
 agent = create_agent(tools=tools, model=llm)
 
-query = "Give me today's weather in San Jose"
+query = "Give me today's weather in San Jose and how may days is it till Christmas?"
 response = agent.invoke({"messages": [("human", query)]})
 
 final_content = response["messages"][-1].content
